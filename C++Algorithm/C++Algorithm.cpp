4916db3da0b6b037bf769e6e5127bfb2bfcbb8dc
LDK <ldk78423@gmail.com>
2024-07-11 19:41:54 +0900

    20240711(브루트 포스 알고리즘)

---
+++ C++Algorithm/C++Algorithm.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+bool Check(int num)
+{
+    if (num == 0 || num == 1)
+    {
+        return false;
+    }
+    for (int i = 2; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int num = 4;
+    if (Check(num) == true)
+    {
+        cout << "소수 확인" << endl;
+    }
+    else
+    {
+        cout << "소수가 아님" << endl;
+    }
+
+    return 0;
+
+}
