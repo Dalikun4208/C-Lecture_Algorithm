87cbee97fd92cbe974c82f7b7908a8e4ef1e53dc
ldk <ldk78423@gmail.com>
2024-07-15 19:42:29 +0900

    20240715(선택,삽입 정렬)
    ㅁㄴㅇ

--- C++Algorithm/C++Algorithm.cpp
+++ C++Algorithm/C++Algorithm.cpp
@@ -2,34 +2,39 @@
 #include <string>
 using namespace std;
 
-bool Check(int num)
-{
-    if (num == 0 || num == 1)
-    {
-        return false;
-    }
-    for (int i = 2; i < num; i++)
-    {
-        if (num % i == 0)
-        {
-            return false;
-        }
-    }
-    return true;
-}
+#define SIZE 5
 
 int main()
 {
-    int num = 4;
-    if (Check(num) == true)
-    {
-        cout << "소수 확인" << endl;
-    }
-    else
-    {
-        cout << "소수가 아님" << endl;
-    }
 
-    return 0;
+#pragma region 삽입 정렬
+
+	//데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여 자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘입니다.
+	int list[SIZE] = { 8, 5, 6, 2, 4 };
+	int key = 0;
+	int j = 0;
+	
+	for (int i = 1; i < SIZE; i++)
+	{
+		key = list[i];
+
+		for (j = i - 1; j >= 0 && list[j] > key; j--)
+		{
+			list[j + 1] = list[j];
+		}
+
+		list[j + 1] = key;
+	}
+
+	for (const int& element : list)
+	{
+		cout << element << " ";
+	}
+	cout << endl;
+
+#pragma endregion
+
+
+	return 0;
 
 }
---
+++ Class4th(SelectSort)/Class4th(SelectSort).cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+#define SIZE 5;
+
+int main()
+{
+
+#pragma region 선택 정렬
+	// 주어진 리스트 중에 최소값을 찾아서 맨 앞에 위치한 결과를 교체하는 방식으로 정렬하는 알고리즘입니다.
+
+	int List[5] = { 6, 5, 2, 8, 10 };
+	int min = 0;
+	int MinIndex = 0;
+
+	cout << "현재 리스트는 : ";
+	for (int i = 0; i < 5; i++)
+	{
+		cout << List[i] << " ";
+	}
+	cout << endl;
+
+
+
+	for (int i = 0; i < 5; i++)
+	{
+		min = List[i];
+		for (int j = i + 1; j < 5; j++)
+		{
+			if (min > List[j])
+			{
+				min = List[j];
+				MinIndex = j;
+			}
+		}
+
+		std::swap(List[i], List[MinIndex]);
+	}
+
+	for (const int& element : List)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+	return 0;
+
+}
