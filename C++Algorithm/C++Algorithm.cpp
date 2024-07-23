bd7e4a280f908a834ce28c8da6360d0632fdcc3b
ldk <ldk78423@gmail.com>
2024-07-23 19:44:34 +0900

    20240723(shell sort)

--- C++Algorithm/C++Algorithm.cpp
+++ C++Algorithm/C++Algorithm.cpp
@@ -2,23 +2,54 @@
 
 using namespace std;
 
+#define SIZE 10
 
+int Fuction(int a, int b)
+{
+    int result = 0;
+    
+    while (true)
+    {
+        int i = 2;
+        while (true)
+        {
+            if (a % i == 0 && b % i == 0)
+            {
+                if (result == 0)
+                {
+                    result = result + i;
+                }
+                else
+                {
+                    result = result * i;
+                }
+                a = a / i;
+                b = b / i;
+                break;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        if (a % i > 0 && b % i > 0)
+        {
+            break;
+        }
+
+    }
+
+    return result;
+}
 
 int main()
 {
+    
+#pragma region 1123
 
-#pragma region 쉘 정렬
-
-    // 먼 거리의 요소들을 먼저 정렬하여 배열을 부분적으로
-    // 정렬한 후, 점진적으로 더 작은 간격을 사용하는 정렬 알고리즘입니다.
-
-
-
+    
+    cout << "값은 : " << Fuction(30, 48) << endl;
 #pragma endregion
-
-
-
-
 
 
     return 0;
