907356eda2397f78e34569c9ef17321718264774
Dalikun4208 <ldk78423@gmail.com>
2024-07-24 19:44:52 +0900

    20240724(hanoi)

--- C++Algorithm/C++Algorithm.cpp
+++ C++Algorithm/C++Algorithm.cpp
@@ -2,55 +2,21 @@
 
 using namespace std;
 
-#define SIZE 10
-
-int Fuction(int a, int b)
-{
-    int result = 0;
-    
-    while (true)
-    {
-        int i = 2;
-        while (true)
-        {
-            if (a % i == 0 && b % i == 0)
-            {
-                if (result == 0)
-                {
-                    result = result + i;
-                }
-                else
-                {
-                    result = result * i;
-                }
-                a = a / i;
-                b = b / i;
-                break;
-            }
-            else
-            {
-                i++;
-            }
-        }
-        if (a % i > 0 && b % i > 0)
-        {
-            break;
-        }
-
-    }
-
-    return result;
-}
 
 int main()
 {
-    
-#pragma region 1123
+#pragma region 백트래킹
+    //해를 찾아가는 도중에 지금의 경로가 해가 될 것 같지 않으면,
+    // 더 이상 깊이 들어가지 않고, 이전 단계로 다시 돌아가는 알고리즘.
 
-    
-    cout << "값은 : " << Fuction(30, 48) << endl;
+    // 백트래킹의 유망성 판단
+    // 해가 될 만한지 판단한 후 유망하지 않다고 결정되면,
+    // 그 노드의 이전 노드로 돌아가 다음 자식 노드로 이동한다.
+
+    // 해가 될 만한 가능성이 있으면 유망하다. (promising)
+    // 유망하지 않은 노드에 가지 않는 것 (pruning)
+
 #pragma endregion
-
 
     return 0;
 }
