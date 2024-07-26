4c42eb03f0c8db5d3bac44f543ca17204f99cbd7
Dalikun4208 <ldk78423@gmail.com>
2024-07-26 19:46:08 +0900

    20240723(bfs)

--- C++Algorithm/C++Algorithm.cpp
+++ C++Algorithm/C++Algorithm.cpp
@@ -1,87 +1,71 @@
 ﻿#include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 #define SIZE 8
 
-void insert(int vertex, int edge)
-{
-
-}
-
 class Graph
 {
-private :
+private:
+    queue<int> queue;
     bool visited[SIZE];
     vector<int> graph[SIZE];
-
-
-public :
+    int list[SIZE];
+public:
     Graph()
     {
         for (int i = 0; i < SIZE; i++)
         {
             visited[i] = false;
+            list[i] = 0;
         }
     }
     void Insert(int vertex, int edge)
     {
         graph[vertex].push_back(edge);
         graph[edge].push_back(vertex);
-
-
     }
+
     void search(int start)
     {
-        visited[start] = true;
-        cout << start << " ";
 
-        for (int i = 0; i < graph[start].size(); i++)
-        {
-            int next = graph[start][i];
-            if (visited[next] == false)
-            {
-                search(next);
-            }
-        }
     }
 };
 
 int main()
 {
 
-#pragma region 깊이 우선 탐색
-
-    // root 노드에서 시작해서 다음 분기로 넘어가기 전에
-    // 해당 분기를 완벽하게 탐색하는 탐색 알고리즘입니다.
-
-    // 1. 시작 노드를 스택에 넣고 방문 처리를 합니다.
-    // 2. 스택의 최상단 노드에 방문하지 않은 인접 노드가 있으면
-    // 그 노드를 스택에 넣고 방문 처리합니다.
-    // 3. 방문하지 않은 인접 노드가 없으면 스택에서 최상단 노드를 꺼냅니다.
-    // 4. 더 이상2번의 과정을 수행할 수 없을때까지 반복합니다.
+#pragma region 위상정렬
+    // 방향 그래프에 존재하는 각 정점들의 선행 순서를 지키며,
+    // 모든 정점을 차례대로 진행하는 알고리즘입니다.
+    // 사이클이 발생하는 경우 위상 정렬을 수행할 수 없습니다.
+    // DAG directed acyclic graph : 사이클이 존재하지 않는 그래프
+    // 시간 복잡도 : O(V + E)
+    // 1. 진입 차수가 0인 정점을 QUEUE에 삽입합니다.
+    // 2. QUEUE에서 원소를 꺼내 연결된 모든 간선을 제거합니다.
+    // 3, 간선 제거 이후에 진입 차수가 0이 된 정점을 QUEUE에 삽입합니다.
+    // 4. QUEUE가 비어있을떄까지 2~3번을 반복 수행한다.
 
     Graph graph;
 
     graph.Insert(1, 2);
-    graph.Insert(1, 3);
+    graph.Insert(1, 5);
 
     graph.Insert(2, 3);
 
-    graph.Insert(2, 4);
-    graph.Insert(2, 5);
+    graph.Insert(5, 6);
 
-    graph.Insert(3, 6);
-    graph.Insert(3, 7);
+    graph.Insert(3, 4);
 
-    graph.Insert(4, 5);
+    graph.Insert(6, 4);
 
-    graph.Insert(6, 7);
+    graph.Insert(4, 7);
 
 
-    graph.search(1);
 
 #pragma endregion
+
 
 
     return 0;
---
+++ class17th(breath first search)/class17th(breath first search).cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+#define SIZE 6
+
+class Graph
+{
+private:
+    queue<int> queue;
+    bool visited[SIZE];
+    vector<int> graph[SIZE];
+public:
+    Graph()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            visited[i] = false;
+        }
+    }
+    void Insert(int vertex, int edge)
+    {
+        graph[vertex].push_back(edge);
+        graph[edge].push_back(vertex);
+    }
+
+    void search(int start)
+    {
+        queue.push(start);
+        visited[start] = true;
+
+        while (queue.empty() == false)
+        {
+            int x = queue.front();
+            queue.pop();
+            cout << x << " ";
+            for (int i = 0; i < graph[x].size(); i++)
+            {
+                int next = graph[x][i];
+                if (visited[next] == false)
+                {
+                    queue.push(next);
+                    visited[next] = true;
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+
+#pragma region 깊이 우선 탐색
+
+    Graph graph;
+
+    graph.Insert(1, 2);
+    graph.Insert(1, 3);
+
+    graph.Insert(2, 4);
+
+    graph.Insert(3, 4);
+    graph.Insert(3, 5);
+
+    graph.Insert(5, 4);
+
+    graph.search(1);
+
+#pragma endregion
+
+
+    return 0;
+}
